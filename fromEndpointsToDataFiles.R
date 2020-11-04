9cabfa60e48585e90a2a15f8c3b606249c08c408
Sara Gosline <sara.gosline@pnnl.gov>
2020-11-04 10:13:23 -0800

    Updated script to add additional extract data

--- fromEndpointsToDataFiles.R
+++ fromEndpointsToDataFiles.R
@@ -14,21 +14,24 @@ require(WikipediR)
 #' 5- ZF curves to plot
 
 
+#These pathways refer to absolute pathways in the docker image
 data.dir='/srpAnalytics/data/'
-#data.dir='data/'
+data.dir='data/'
 out.dir='/tmp/'
-#out.dir='./'
+out.dir='./'
 
-#step 1 - get cas ids to class
+#step 1 - get cas ids to pre-defined class
 chemMapping<-read.csv(paste0(data.dir,'Chemicals.csv'))%>%
                       dplyr::select(cas_number,chemical_class,zaap_cid)%>%
                       distinct()%>%
                       rename(zf.cid='zaap_cid')
 
+#this file was downloaded from osu
 newMapping<-readxl::read_xlsx(paste0(data.dir,'chemicalid_table_2020AUG07.xlsx'))%>%
     select(cas_number='casrn',zf.cid='chemical_id')%>%
     mutate(chemical_class=NA)
 
+#here is more mapping information from OSU
 fullMapping<-readxl::read_xlsx(paste0(data.dir,'OSU_BU_Overlap_Inventory_2020MAR30.xlsx'))%>%
     select(casrn,zf.cid)%>%
     distinct()%>%
@@ -39,11 +42,15 @@ fullMapping<-readxl::read_xlsx(paste0(data.dir,'OSU_BU_Overlap_Inventory_2020MAR
     rename(cas_number='casrn')%>%
     rbind(newMapping)
 
-
+#here is our pre-defined dictionary
 endpointDetails<-readxl::read_xlsx(paste0(data.dir,'SuperEndpoint Mapping 2020Sept2.xlsx'),
                                    sheet='Dictionary')%>%
     rename(End_Point='Abbreviation',`End Point Name`='Simple name (<20char)')
 
+
+#'getNewChemicalClass
+#'This file reads in the files and processes the chemical class names
+#'to be friendly for the website
 getNewChemicalClass<-function(){
   pahs=readxl::read_xlsx(paste0(data.dir,'/PAH_and_1530_SRP_Summary.xlsx'),sheet='Master summary-PAHs')%>%
       dplyr::select(cas_number='casrn')%>%
@@ -69,6 +76,7 @@ getNewChemicalClass<-function(){
 
 }
 
+#here we join the chemical metadata from the comptox dashboard
 chemMeta<-readxl::read_xls(paste0(data.dir,
                                   '/CompToxChemicalsDashboard-Batch-Search_2020-08-27_17_26_49.xls'))%>%
     select(INPUT,DTXSID,PREFERRED_NAME,INCHIKEY,SMILES,MOLECULAR_FORMULA,
@@ -81,54 +89,79 @@ chemMeta<-readxl::read_xls(paste0(data.dir,
     select(-c(chemical_class,Classification))%>%
     rename(chemical_class='newClass')
 
-chemMeta$zf.cid<-unlist(as.character(chemMeta$zf.cid))
+chemMeta<-mutate(chemMeta,Chemical_ID=unlist(as.character(zf.cid)))
 
-#full characterization of sample data with ids
+#These files have the extract information and the mappin gof extracts to chemicals
 sampMeta<-readxl::read_xlsx(paste0(data.dir,'/superfund_location_data.xlsx'))
 sampChem<-read.csv(paste0(data.dir,'/curatedSampData.csv'))%>%
-    select(chem_lims_num,date_sampled,sample_matrix,technology,zaap_cid,extract_exp,
+    select(chem_lims_num,date_sampled,sample_matrix,technology,Sample_ID='zaap_cid',
            zf_lims_id,measurement_value,unit,cas_number)%>%distinct()%>%
   rename(SampleNumber='chem_lims_num')%>%left_join(sampMeta)
 
+#'combineChemicalEndpointData
+#'@param bmdfiles a list of files that come from the BMD pipeline
+#'@param is_extract - if our chemical id is an extract we use a different metadata
+#'@return a data.frame 
 ##We will release an 'endpoint file for each condition'
-combineChemicalEndpointData<-function(bmdfiles){
+combineChemicalEndpointData<-function(bmdfiles,is_extract=FALSE){
 
   files = lapply(bmdfiles,read.csv)
-  full.bmd<-do.call(rbind,files)%>%
+  
+  mid.bmd<-do.call(rbind,files)%>%
 #    filter(!is.null(BMD_Analysis_Flag))%>%
     dplyr::select(Chemical_ID,End_Point,Model,BMD10,BMD50,AUC_Norm,BMD10_Flag,BMD50_Flag)%>%
-    mutate(zf.cid=as.character(Chemical_ID),endPointLink='')%>%
-    left_join(chemMeta)%>%
-    rename(ChemicalId='zf.cid')%>%
-    left_join(endpointDetails)%>%
-    distinct()%>%select(-ChemicalId)
+    mutate(endPointLink='')
+    
+  
+  if(is_extract)
+    full.bmd<-mid.bmd%>%mutate(Sample_ID=as.character(Chemical_ID))%>%
+      left_join(sampChem)%>%#%>%mutate(Chemical_ID=as.character(zaap_cid)))%>%
+      left_join(endpointDetails)%>%
+      select(-c(Chemical_ID,End_Point,cas_number,measurement_value,unit))%>%
+    distinct()
+  
+  else
+    full.bmd<-mid.bmd%>%
+    mutate(Chemical_ID=as.character(Chemical_ID))%>%
+      left_join(chemMeta)%>%
+#      rename(Chemical_ID='zf.cid')%>%
+      left_join(endpointDetails)%>%
+      distinct()%>%select(-c(End_Point))##should we remove endpoint YES
 
   return(full.bmd)
 }
 
 
+#'combineChemicalFit files 
+#'@param bmdfiles the output of files from teh BMD pipeline describing the dose response 
+# @return data.frame
 ##We will release an 'endpoint file for each condition'
-combineChemicalFitData<-function(bmdfiles){
+combineChemicalFitData<-function(bmdfiles, is_extract=FALSE){
 
   files = lapply(bmdfiles,read.csv)
+  
   full.bmd<-do.call(rbind,files)%>%
     mutate(zf.cid=as.character(Chemical_ID))%>%
     rename(ChemicalId='zf.cid')%>%
      left_join(endpointDetails)%>%
      distinct()%>%
-    select(-c(`End Point Name`,Description,ChemicalId))
-
+    select(-c(End_Point,Description,ChemicalId))
+  if(is_extract)
+    full.bmd <- rename(full.bmd,Sample_ID='Chemical_ID')
   return(full.bmd)
 }
 
-combineChemicalDoseData<-function(bmdfiles){
+combineChemicalDoseData<-function(bmdfiles, is_extract=FALSE){
 
   files = lapply(bmdfiles,read.csv)
   full.bmd<-do.call(rbind,files)%>%
     mutate(zf.cid=as.character(Chemical_ID))%>%
     rename(ChemicalId='zf.cid')%>%
-  #  left_join(endpointDetails)%>%
+   left_join(endpointDetails)%>%
+    dplyr::select(-c(End_Point,Description))%>%
     distinct()%>%select(-ChemicalId)
+  if(is_extract)
+    full.bmd <- rename(full.bmd,Sample_ID='Chemical_ID')
 
   return(full.bmd)
 }
@@ -151,6 +184,12 @@ curv.files <-c(curv.files,paste0(data.dir,'/fit_vals_extra_ep.csv'))
 dose.files <-c(dose.files,paste0(data.dir,'/dose_response_vals_extra_ep.csv'))
 
 
+#extract data
+e.bmd<-paste0(data.dir,'/bmd_vals_extracts_sara.csv')
+e.curv<-paste0(data.dir,'/fit_vals_extracts.csv')
+e.dose<-paste0(data.dir,'/dose_response_vals_extracts.csv')
+
+
 ##now we check for additional files
 parser = argparse::ArgumentParser()#'Command line tool to add extract-dose-response data to SRP Analytics Portal')
 parser$add_argument('--bmd',dest='new.bmd',type='character',default='',help='BMD file to add to portal')
@@ -170,7 +209,12 @@ bmds<-combineChemicalEndpointData(bmd.files)
 curves <-combineChemicalFitData(curv.files)
 doseReps <-combineChemicalDoseData(dose.files)
 
+ebmds<-combineChemicalEndpointData(c(e.bmd),TRUE)
+ecurves <- combineChemicalFitData(c(e.curv),TRUE)
+edrs<- combineChemicalDoseData(c(e.dose),TRUE)
+
 id_mapping <-bmds%>%select(Chemical_ID,cas_number,AVERAGE_MASS)%>%distinct()
+
 sampChem <-sampChem%>%left_join(id_mapping)%>%
   mutate(unit=stringr::str_replace_all(unit,'ug/ml','pg/uL'))%>%
   mutate(unit=stringr::str_replace_all(unit,'pg/ul','pg/uL'))%>%
@@ -181,12 +225,19 @@ sampChem <-sampChem%>%left_join(id_mapping)%>%
   dplyr::select(-c(concentration,AVERAGE_MASS,unit))
 
 
+##Final output for the platform team is these 4 files
 write.csv(bmds,file=paste0(out.dir,'chemSummaryStats.csv'),row.names = FALSE)
+write.csv(ebmds,file=paste0(out.dir,'envSampSummaryStats.csv'),row.names=FALSE)
+
 write.csv(curves,file=paste0(out.dir,'chemXYcoords.csv'),row.names = FALSE)
+write.csv(ecurves,file=paste0(out.dir,'envSampXYcoords.csv'),row.names = FALSE)
+
 write.csv(doseReps,file=paste0(out.dir,'chemdoseResponseVals.csv'),row.names = FALSE)
+write.csv(edrs,file=paste0(out.dir,'envSampdoseResponseVals.csv'),row.names = FALSE)
+
+
 write.csv(sampChem,file=paste0(out.dir,'chemicalsByExtractSample.csv'),row.names=FALSE)
 
-#chemsOnly<-bmds%>%dplyr::select(Chemical_ID,cas_number,PREFERRED_NAME,chemical_class)%>%distinct()
-#write.csv(chemsOnly,'chemsWithEndpoints.csv',row.names=F)
+##TODO: zip them up into package together with a readme/description for datahub 
 
-##do we have curves for the extract data? if so we need to merge that as well
+
