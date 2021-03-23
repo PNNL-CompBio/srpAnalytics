307c7c437869221b5514e3f6eea70cb12f2910c9
Doo Nam Kim <doonam.kim@gmail.com>
2021-03-23 13:58:13 -0700

    for constance

--- analysis/latest/3_bmd_feasibility_BMD/each/Phase_I_II/LPR/wide2dicho_LPR_phase_I_II_t0_t239_display_to_print.py
+++ analysis/latest/3_bmd_feasibility_BMD/each/Phase_I_II/LPR/wide2dicho_LPR_phase_I_II_t0_t239_display_to_print.py
@@ -29,7 +29,6 @@ code_location = os.path.dirname(os.path.abspath(py_file))
 index_of_latest = code_location.index('latest')
 util_path = os.path.join(code_location[:index_of_latest], "latest", "util")
 print ("util_path:" + str(util_path))
-a=b
 #'''
 
 sys.path.insert(0, util_path)
