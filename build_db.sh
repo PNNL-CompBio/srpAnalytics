c0e3c14263450a9ad926f0601df1f81bd95f4a1f
Sara Gosline <sara.gosline@pnnl.gov>
2021-04-28 09:28:23 -0700

    updated build script

--- build_db.sh
+++ build_db.sh
@@ -1,2 +1,9 @@
-docker build . -t srp-analytics
-docker run -v $PWD:/tmp srp-analytics to_be_processed/7_PAH_zf_morphology_data_2020NOV11_tall.csv,to_be_processed/7_PAH_zf_morphology_data_2021_Jan11.csv
+#!/bin/bash
+#build database script
+build='/usr/bin/docker build . -t srp-analytics'
+echo $build
+$build
+
+run='/usr/bin/docker run -v '$PWD':/tmp srp-analytics to_be_processed/7_PAH_zf_morphology_data_2020NOV11_tall.csv,to_be_processed/7_PAH_zf_morphology_data_2021_Jan11.csv'
+echo $run
+$run
