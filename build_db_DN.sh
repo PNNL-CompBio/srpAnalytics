7df226b8049d98cb0c19dfffee27e7393f23b1e8
Doo Nam Kim <doonam.kim@gmail.com>
2021-07-15 09:09:43 -0700

    co to main related br

---
+++ build_db_DN.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#build database script
+build="docker build . -t srp-analytics"
+echo $build
+$build
+
+f1=to_be_processed/7_PAH_zf_morphology_data_2020NOV11_tall_3756.csv
+#f2=to_be_processed/7_PAH_zf_morphology_data_2021_Jan11.csv
+run="docker run -v "$PWD":/tmp srp-analytics "$f1" --devel"
+echo $run
+$run
