029e72a213927fab5ffcf4861585e93612adc6c9
Doo Nam Kim <doonam.kim@gmail.com>
2021-07-15 08:43:58 -0700

    use 1 chemical for docker run

--- build_db_DN.sh
+++ build_db_DN.sh
@@ -4,8 +4,9 @@ build="docker build . -t srp-analytics"
 echo $build
 $build
 
-f1=to_be_processed/7_PAH_zf_morphology_data_2020NOV11_tall.csv
-#f2=to_be_processed/7_PAH_zf_morphology_data_2021_Jan11.csv
+#f1=to_be_processed/7_PAH_zf_morphology_data_2020NOV11_tall.csv
+f1=to_be_processed/7_PAH_zf_morphology_data_2020NOV11_tall_3756.csv
+
 run="docker run -v "$PWD":/tmp srp-analytics "$f1" --devel"
 echo $run
 $run
