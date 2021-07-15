7605150388b40c8dffc616be0e6c3d6a4a7b5093
Sara Gosline <sara.gosline@pnnl.gov>
2021-07-15 08:32:00 -0700

    Updated chemical id mapping

--- buildv1database.R
+++ buildv1database.R
@@ -53,7 +53,7 @@ getChemMetadata<-function(data.dir){
     ##this file comes from COMPTOX
     ##here we join the chemical metadata from the comptox dashboard
     chemMeta<-readxl::read_xls(paste0(data.dir,
-                                      'CompToxChemicalsDashboard-Batch-Search_2021-04-28_10_10_47.xls'))%>%
+                                      'CompToxChemicalsDashboard-Batch-Search_2021-07-14_17_49_42.xls'))%>%
         select(INPUT,DTXSID,PREFERRED_NAME,INCHIKEY,SMILES,MOLECULAR_FORMULA,
                AVERAGE_MASS,PUBCHEM_DATA_SOURCES)%>%
         rename(cas_number='INPUT')%>%
