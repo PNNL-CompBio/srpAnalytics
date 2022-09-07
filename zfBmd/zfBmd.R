18ffaf30bf76f9b514f188ad2625ef482fb9e492
david.degnan@pnnl.gov <degn400@we45748.pnl.gov>
2022-09-07 14:53:59 -0700

    Last commit before moving to scripts

--- zfBmd/zfBmd.R
+++ zfBmd/zfBmd.R
@@ -29,111 +29,16 @@ toRM <- c('3756 0 20544 H02', '3756 0 20544 H08', '3756 0 20624 H05',
 LPR_timepoints <- fread("~/Git_Repos/srpAnalytics/zfBmd/test_files/7_PAH_zf_LPR_data_2021JAN11_3756.csv")
 
 
-LPR_timepoints <- LPR_timepoints %>%
-  mutate(
-    time = (gsub("t", "", variable) %>% as.numeric()) / 10,
-    well.id = paste(chemical.id, conc, plate.id, well),
-    plate.id = as.factor(plate.id)
-  ) %>%
-  filter(well.id %in% toRM == FALSE) 
-
 #############
 ## QC PLOT ##
 #############
 
-plot_data <- LPR_timepoints %>% 
-  group_by(time, conc, plate.id) %>%
-  summarise(
-    NewVal = sum(value, na.rm = T)
-  ) 
-
-ggplot(plot_data, aes(x = time, y = NewVal, color = plate.id)) + ylab("Movement") +
- xlab("Minutes") + theme_bw() + ggtitle("Chemical ID = 3756. Wells are summed together.") +
- annotate(geom = "rect", xmin = c(0, 6, 12, 18), xmax = c(2, 8, 14, 20), 
-          ymin = rep(-5, 4), ymax = rep(380, 4),
-          fill = "yellow", color = NA, alpha = 0.75) +
- annotate(geom = "rect", xmin = c(3, 9, 15, 21), xmax = c(5, 11, 17, 23), 
-          ymin = rep(-5, 4), ymax = rep(380, 4),
-          fill = "gray50", color = NA, alpha = 0.5) +
- geom_line() + facet_wrap(~conc)
-
- 
-###################
-## CALCULATE MOV ##
-###################
-
-# MOV is the difference between the start of the dark cycle and the end of the light cycle
-# Here, the light cycles end at 2, 8, 14, and 20; and the dark cycles end at 5, 11, 17, 23. 
-
-Num_Cycles = 4 
-Assumed_Cycle_Time = 3 
-Assumed_Light_End = 2
-Assumed_Dark_Begin = 3
-
-MOV <- do.call(rbind, lapply(1:Num_Cycles, function(num) {
-  
-  Value <- (LPR_timepoints[LPR_timepoints$time == (Assumed_Dark_Begin + (Assumed_Cycle_Time * num - 1)), "value"] - 
-    LPR_timepoints[LPR_timepoints$time == (Assumed_Light_End + (Assumed_Cycle_Time * num - 1)), "value"]) %>% unlist()
-  
-  Res <- LPR_timepoints[LPR_timepoints$time == Assumed_Light_End, c("chemical.id", "conc", "plate.id", "well")] %>%
-    mutate(
-      endpoint = paste0("MOV", num),
-      Value = Value
-    )
-  
-  return(Res)
-  
-}))
-
-################################
-## CONVERT MOV TO DICHOTOMOUS ##
-################################
-
-# Negative control is the concentration at 0. If more than 50% of the abnormal (hypo) remain,
-# then remove the entire plate 
-MOV_0 <- MOV %>%
-  filter(conc == 0) %>%
-  group_by(chemical.id, plate.id, endpoint) %>%
-  mutate(
-    Quart1 = quantile(Value[Value > 0 & !is.na(Value)], 0.25),
-    Quart3 = quantile(Value[Value > 0 & !is.na(Value)], 0.75),
-    IQR_A = (Quart3 - Quart1) * 1.5
-  ) %>%
-  summarise(
-    Hypo = sum(Value < 0, na.rm = T),
-    Hyper = sum(Value <= (Quart1 - IQR_A) | Value >= (Quart3 + IQR_A), na.rm = T),
-    Total = length(Value),
-    ThresholdAt0 = Total * 0.5,
-    Remove = Hyper < ThresholdAt0 
-  ) %>%
-  dplyr::mutate(NewID = paste(chemical.id, plate.id, endpoint))
-
-PlatesToRm <- MOV_0 %>% filter(Remove) %>% ungroup() %>% select(NewID) %>% unlist()
-
-# Calculate final response
-MOV_Final <- MOV %>%
-  mutate(NewID = paste(chemical.id, plate.id, endpoint)) %>%
-  filter(NewID %in% PlatesToRm == FALSE) %>%
-  group_by(chemical.id, conc, endpoint) %>%
-  summarise(
-    Hypo = sum(Value < 0, na.rm = T),
-    Total = length(Value),
-    Response = Hypo / Total
-  )
-
-# Add that MOV 1 is missing
-ggplot(MOV_Final, aes(x = conc, y = Response, color = endpoint)) + geom_line() + theme_bw()
-
-###################
-## CALCULATE AUC ##
-###################
-
-#  Summation
-LPR_timepoints <- LPR_timepoints %>%
+Summed <- LPR_timepoints %>%
   mutate(
     time = (gsub("t", "", variable) %>% as.numeric()) / 10,
     TimeBin = floor(time),
-    well.id = paste(chemical.id, conc, plate.id, well)
+    well.id = paste(chemical.id, conc, plate.id, well),
+    plate.id = as.factor(plate.id)
   ) %>%
   filter(
     well.id %in% toRM == FALSE
@@ -144,36 +49,129 @@ LPR_timepoints <- LPR_timepoints %>%
   ) %>%
   ungroup() 
 
-ggplot(LPR_timepoints, aes(x = TimeBin, y = Sum)) + geom_smooth() + xlab("Minutes") + theme_bw() +
-  geom_vline(xintercept = c(2, 3), color = "black") + facet_wrap(.~conc) + ylab("Summed LPR, n = 3")
+ggplot(Summed, aes(x = TimeBin, y = Sum, color = plate.id)) + ylab("Movement") +
+  xlab("Minutes") + theme_bw() + ggtitle("Chemical ID = 3756. Wells are summed together.") +
+  annotate(geom = "rect", xmin = c(0, 6, 12, 18), xmax = c(2, 8, 14, 20), 
+           ymin = rep(-5, 4), ymax = rep(380, 4),
+           fill = "yellow", color = NA, alpha = 0.75) +
+  annotate(geom = "rect", xmin = c(3, 9, 15, 21), xmax = c(5, 11, 17, 23), 
+           ymin = rep(-5, 4), ymax = rep(380, 4),
+           fill = "gray50", color = NA, alpha = 0.5) +
+  geom_line() + facet_wrap(~conc)
+
+
+###################
+## CALCULATE MOV ##
+###################
+
+# MOV is the difference between the start of the dark cycle and the end of the light cycle
+# Here, the light cycles end at 2, 8, 14, and 20; and the dark cycles end at 5, 11, 17, 23. 
 
 # MOV1 (actually MOV2) is the movement at the start of light cycle 2
-MOV_test <- LPR_timepoints %>%
+MOV <- Summed %>%
   filter(TimeBin %in% c(2,3)) %>%
   group_by(chemical.id, conc, plate.id, well) %>%
   summarise(
-    MOV = Sum[TimeBin == 3] - Sum[TimeBin == 2]
+    Response = Sum[TimeBin == 3] - Sum[TimeBin == 2]
   ) 
 
-test <- MOV_test %>%
+ggplot(MOV, aes(x = as.factor(conc), y = Response)) + geom_boxplot()
+
+################################
+## CONVERT MOV TO DICHOTOMOUS ##
+################################
+
+MOV_Final <- MOV %>%
   ungroup() %>%
   group_by(chemical.id, conc, plate.id) %>%
   mutate(
-    Quart1 = quantile(MOV[which(conc == 0)], 0.25), 
-    Quart3 = quantile(MOV[which(conc == 0)], 0.75),
-    IQR_A = (Quart3 - Quart1) * 1.5
+    Quart1 = quantile(Response[Response > 0], 0.25), 
+    Quart3 = quantile(Response[Response > 0], 0.75),
+    IQR_A = (Quart3 - Quart1) * 1.5, 
+    Hypo = Response <= 0,
+    Hyper = Response > 0 & (Response < (Quart1 - IQR_A) | Response > (Quart3 + IQR_A))
   ) %>%
-  summarise(
-    Hypo = sum(MOV <= 0, na.rm = T),
-    Hyper = sum(MOV < (Quart1 - IQR_A) | MOV > (Quart3 + IQR_A), na.rm = T),
-    Total = length(MOV)
-  ) %>%
-  ungroup() %>%
   group_by(chemical.id, conc) %>%
   summarise(
     Hypo = sum(Hypo, na.rm = T),
     Hyper = sum(Hyper, na.rm = T),
-    Total = sum(Total),
-    Response = (Hypo + Hyper) / Total
+    Total = length(Response),
+    Response =  Hypo / Total
   )
 
+# Add that MOV 1 is missing
+ggplot(MOV_Final, aes(x = conc, y = Response)) + geom_line() + theme_bw() + 
+  ggtitle("MOV")
+
+###################
+## CALCULATE AUC ##
+###################
+
+
+AUC <- do.call(rbind, lapply(1:Num_Cycles, function(num) {
+  
+  TimeLapse <- Assumed_Cycle_Time * (num - 1)
+
+  Light <- LPR_timepoints %>% 
+    group_by(chemical.id, plate.id, conc, well) %>%
+    summarise(
+      Value = sum(value[time %in% (Assumed_Light_Begin + TimeLapse):(Assumed_Light_End + TimeLapse)], na.rm = T)
+    )
+  
+  Dark <-  LPR_timepoints %>% 
+    group_by(chemical.id, plate.id, conc, well) %>%
+    summarise(
+      Value = sum(value[time %in% (Assumed_Dark_Begin + TimeLapse):(Assumed_Dark_End + TimeLapse)], na.rm = T)
+    )
+
+  Res <- Light[,c("chemical.id", "conc", "plate.id", "well")] %>%
+    ungroup() %>%
+    mutate(
+      endpoint = paste0("AUC", num),
+      Value = (Dark$Value - Light$Value) %>% unlist()
+    )
+  
+  return(Res)
+  
+}))
+
+ggplot(AUC, aes(x = as.factor(conc), y = Value, fill = endpoint)) + geom_boxplot()
+
+################################
+## CONVERT AUC TO DICHOTOMOUS ##
+################################
+
+# Negative control is the concentration at 0. If more than 50% of the abnormal (hypo) remain,
+# then remove the entire plate 
+AUC_0 <- AUC %>%
+  filter(conc == 0) %>%
+  group_by(chemical.id, endpoint) %>%
+  mutate(
+    Quart1 = quantile(Value[Value > 0 & !is.na(Value)], 0.25),
+    Quart3 = quantile(Value[Value > 0 & !is.na(Value)], 0.75),
+    IQR_A = (Quart3 - Quart1) * 1.5
+  ) %>%
+  group_by(chemical.id, endpoint, plate.id) %>%
+  summarise(
+    Hypo = sum(Value < 0, na.rm = T),
+    Hyper = sum(Value > 0 & (Value >= (Quart1 - IQR_A) | Value <= (Quart3 + IQR_A)), na.rm = T),
+    Total = length(Value),
+    ThresholdAt0 = Total * 0.5,
+    Remove = Hyper < ThresholdAt0 
+  ) %>%
+  dplyr::mutate(NewID = paste(chemical.id, plate.id, endpoint))
+
+PlatesToRm <- AUC_0 %>% filter(Remove) %>% ungroup() %>% select(NewID) %>% unlist()
+
+AUC_Final <- AUC %>%
+  mutate(NewID = paste(chemical.id, plate.id, endpoint)) %>%
+  filter(NewID %in% PlatesToRm == FALSE) %>%
+  group_by(chemical.id, conc, endpoint) %>%
+  summarise(
+    Hypo = sum(Value < 0, na.rm = T),
+    Total = length(Value),
+    Response = Hypo / Total
+  )
+
+ggplot(AUC_Final, aes(x = conc, y = Response, color = endpoint)) + geom_line() + theme_bw() +
+  ggtitle("AUC")
