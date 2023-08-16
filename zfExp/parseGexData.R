1e39ed76845690bf7811b34100a35f4724cb5adf
sgosline <sara.gosline@pnnl.gov>
2023-08-16 10:20:15 -0700

    added more gene mapping info and visualiation ideas

--- zfExp/parseGexData.R
+++ zfExp/parseGexData.R
@@ -11,14 +11,20 @@ schema<-data.frame(Project=c(),
                    nGenes=c(),
                    Chemical_ID=c())
 
+
 tab<-readxl::read_xlsx('./ZF_gex.xlsx',skip=1)
 
 chem<-read.csv('./chemicals.csv')|>
   dplyr::select(Chemical_ID,cas_number)|>distinct()
 
-ind_res<-tab%>% tidyr::pivot_longer(ends_with("Indication"),names_to='condition',values_to='indication')%>%
+ind_res<-tab%>% tidyr::pivot_longer(ends_with("Indication"),
+                                    names_to='condition',values_to='indication')%>%
   dplyr::select(GeneID,Gene,condition,indication)%>%
   mutate(condition=stringr::str_remove(condition,'_DEG_Indication'))
+
+##gene info - need to get basic info about gene and link to zfin db
+geneinfo<-readr::read_csv('allianceGenomeInfo.csv',col_names=c('zfinId','secondId','symbol','name','organism','description'))
+
 
 fc_res<-tab%>% 
   tidyr::pivot_longer(ends_with("Log2FoldChange"),names_to='condition',values_to='Log2FoldChange')%>%
@@ -36,6 +42,12 @@ full_tab<-ind_res%>%
 
 ##went into paper, got them from this table
 ##https://www.mdpi.com/1422-0067/20/10/2570
+
+##one concentration is in ng/mL
+## it is for 1746-01-6 (chem 283) - and mW is 322 g/mol
+## so 1ng/mL = 1000 ug/mL 322 ~ 3 uM
+## ( µg/mL ) = ( µM ) * ( MW in KD) 
+
 treat_names=data.frame(treatment=c('Acenaph_16PAH','BbF_16PAH', 'BjF_16PAH','BkF_16PAH',
                                    'DBahP_16PAH','DBaiP_16PAH','Fluoran_16PAH','Phenan_16PAH',
                                    'Retene_16PAH', 'Anthrac_16PAH','Carbazole_16PAH','X3.NF_16PAH',
@@ -47,7 +59,7 @@ treat_names=data.frame(treatment=c('Acenaph_16PAH','BbF_16PAH', 'BjF_16PAH','BkF
                                    'TDBPP_FRC','TPP_FRC','TiBP_FRC'),
                        Conc=c('50uM','50uM','14.9uM','1.9uM','10uM','5uM','50uM','12.2uM',
                               '50uM','50uM','1.9uM','16.2uM','50uM','50uM','1uM','10uM',
-                              '10uM','2uM','10uM','10uM','1ng/mL','85uM','19.8uM','4uM',
+                              '10uM','2uM','10uM','10uM','3uM','85uM','19.8uM','4uM',
                               '4uM','72uM','85uM','85uM','3uM','8uM','158uM'),
                        cas_number=c('83-32-9','205-99-2','205-82-3','207-8-9','189-64-0','189-55-9','206-44-0','65996-93-2',
                                     '483-65-8','120-12-7','86-74-8','892-21-7','5737-13-3','779-02-2','50-32-8','50-32-8',
@@ -62,7 +74,9 @@ allgenes<-full_tab|>
   left_join(treat_names)|>
   dplyr::select(-c(control,treatment))|>
   mutate(Project='Zebrafish',Link='')|>
-  left_join(chem)
+  left_join(chem)|>
+  mutate(Concentration=as.numeric(stringr::str_replace(Conc,'uM','')))|>
+  select(-Conc)
   
 
 diffex <- allgenes|>
@@ -70,12 +84,19 @@ diffex <- allgenes|>
 
 res<-allgenes|>
   subset(indication!=0)|>
-  group_by(Project,cas_number,Conc,Link,Chemical_ID)|>
-  summarize(nGenes=n())
+  mutate(up=Log2FoldChange>0)|>
+  group_by(Project,cas_number,Concentration,Link,Chemical_ID,up)|>
+  summarize(nGenes=n())|>subset(!is.na(up))
 
-              
+
+res<-res|>
+  tidyr::pivot_wider(names_from='up',
+                     values_from='nGenes')|>
+  rename(UpRegulatedGenes='TRUE',DownRegulatedGenes='FALSE')
+
+
 ##need to get mapping to drug name
-write.table(res,file ='/tmp/srpDEGstats.csv',sep=',',quote=F,row.names=F,col.names=T)
-write.table(allgenes,file='/tmp/allGeneEx.csv',sep=',',quote=F,row.names=F,col.names=T)
+write.table(res,file ='srpDEGstats.csv',sep=',',quote=F,row.names=F,col.names=T)
+write.table(allgenes,file='allGeneEx.csv',sep=',',quote=F,row.names=F,col.names=T)
   
   
