08142a1b18259ced16597ca60c69bc17639046e2
david.degnan@pnnl.gov <degn400>
2024-10-24 09:46:08 -0700

    First step to updating main script with bmdrc functions

--- zfBmd/main.py
+++ zfBmd/main.py
@@ -9,16 +9,7 @@
 import pandas as pd
 import sys
 import argparse
-
-# Import zfBMD specific functions
-from format_binary_data import pre_launch_cleaning 
-from format_binary_data import format_morpho_input
-from format_binary_data import format_lpr_input 
-from calculate_BMD_flags import generate_BMD_flags
-from select_and_run_models import model_fitting
-from select_and_run_models import export_BMDs
-from select_and_run_models import export_fits
-from select_and_run_models import export_doses
+from bmdrc import BinaryClass, LPRClass 
 
 ###########################
 ## COLLECT CLI ARGUMENTS ##
@@ -26,22 +17,27 @@ from select_and_run_models import export_doses
 
 parser = argparse.ArgumentParser('Run the QC and BMD analysis for the SRP analytics compendium')
 
-parser.add_argument('--morpho', dest='morpho', nargs = "+", \
-                    help='Pathway to the morphological file to be processed. Required.',\
-                    default=None)
-parser.add_argument('--LPR', dest='lpr', nargs = "+", \
-                    help='Pathway to the light photometer response (LPR) file to be processed containing the same \
+parser.add_argument('--morpho', dest = 'morpho', nargs = "+", \
+                    help = 'Pathway to the morphological file to be processed. Required. \
+                            Assumed and needed column names are: chemical.id, conc, plate.id, well, variable, value. \
+                            Data assumed to be in long format.',\
+                    default = None)
+parser.add_argument('--LPR', dest = 'lpr', nargs = "+", \
+                    help = 'Pathway to the light photometer response (LPR) file to be processed containing the same \
                           samples as morpho. Optional. Unless both is True, only LPR data will be returned.',\
-                    default=None)
-parser.add_argument('--both', dest='both', \
-                    help='Return both morpho and LPR endpoints. Optional. Default is False.',\
-                    default=False)
+                    default = None)
+parser.add_argument('--both', dest = 'both', \
+                    help = 'Return both morpho and LPR endpoints. Optional. Default is False.',\
+                    default = False)
 parser.add_argument('--output', dest = 'output', \
                     help = 'The output folder for files. Default is current directory.',\
                     default = '.')
-parser.add_argument('--test', dest='test',\
-                    help='Set this flag to test code with internal files. Default is False.',\
-                    action='store_true', default=False)
+parser.add_argument('--test', dest = 'test',\
+                    help = 'Set this flag to test code with internal files. Default is False.',\
+                    action = 'store_true', default=False)
+parser.add_argument('--report', dest ='rep', \
+                    help = 'Generate a report in the output folder. Default is True',
+                    action = 'store_true', default = True)
                     
 ##############################
 ## DEFINE AND RUN FUNCTIONS ##
@@ -79,25 +75,41 @@ def main():
         morpho_paths = './test_files/7_PAH_zf_morphology_data_2020NOV11_tall_3756.csv'
         lpr_paths = './test_files/7_PAH_zf_LPR_data_2021JAN11_3756.csv'
 
-    ### 0. Pre-launch data cleaning-------------------------------------------------------------
+    ### 0. Pre-launch combination-------------------------------------------------------------
 
-    # Remove morphology duplicates with a simple "unique"
-    print("...Removing duplicates in morphology data")
-    morpho_data = pre_launch_cleaning(morpho_paths, "morphology")
+    print("...concatenating datasets")
 
-    # Remove LPR duplicates by converting NA to O, uniquing, and averaging total duplicates
+    # Combine datasets
+    def combine_datasets(thePaths):
+
+        # Create a vector to hold data
+        theData = []
+
+        # Read all files 
+        for thePath in thePaths:
+            theData.append(pd.read_csv(thePath))
+
+        # Combine dataframes 
+        theData = pd.concat(theData)
+        return(theData)
+
+    # Morpho data always needs to be provided. Concatenate datasets together. 
+    morpho_data = combine_datasets(morpho_paths)
+
+    # LPR data is optional. Concatenate datasets together. 
     if (args.lpr is not None or (args.test and args.both)):
-        print("...Removing duplicates in LPR data")
-        lpr_data = pre_launch_cleaning(lpr_paths, "lpr")
-
+        print("...LPR data detected")
+        lpr_data = combine_datasets(lpr_paths)
 
     ### 1. Format data--------------------------------------------------------------------------
+    
     print("...Formatting morphology data")
-    dose_response, theEndpoints, MortWells, Mort24Wells = format_morpho_input(morpho_data)
+    BC = BinaryClass(df = morpho_data, chemical = "chemical.id", concentration = "conc", plate = "plate.id" \
+                     well = "well", endpoint = "variable", value = "value", format = "long")
 
     if (args.lpr is not None or (args.test and args.both)):
         print("...Formatting LPR data")
-        lpr_dose_response = format_lpr_input(lpr_data, theEndpoints, MortWells, Mort24Wells)
+        LPR = LPRClass()
 
     ### 2. Calculate dose response--------------------------------------------------------------
 
